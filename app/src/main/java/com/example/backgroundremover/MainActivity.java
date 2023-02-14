8e2453458532c34135071a2ad2e26d0c7ca50e30
Tamzeed Hossain <tamzeedhossainisu@gmail.com>
2023-02-15 02:17:45 +0600

    first commit

--- app/src/main/java/com/example/backgroundremover/MainActivity.java
+++ app/src/main/java/com/example/backgroundremover/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.backgroundremover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_next);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = (new Intent(MainActivity.this,Home.class));
+                finish();
+            }
+        });
     }
 }
