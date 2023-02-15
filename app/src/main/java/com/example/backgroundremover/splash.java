4eafbb85f8a44206e4a6fe72f51766ad2e180b4d
Tamzeed Hossain <tamzeedhossainisu@gmail.com>
2023-02-15 18:21:01 +0600

    first commit

--- app/src/main/java/com/example/backgroundremover/splash.java
+++ app/src/main/java/com/example/backgroundremover/splash.java
@@ -2,13 +2,30 @@ package com.example.backgroundremover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Code here
+                Intent myIntent = new Intent(splash.this, MainActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        },3100);
     }
 }
